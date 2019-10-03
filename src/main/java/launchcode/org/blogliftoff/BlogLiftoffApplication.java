498e2c64ff6a9d42af5345331e7d57a11caa54f5
Lien Ryan <lienryanstl@gmail.com>
2019-10-03 16:41:49 -0500

    Setup Project

---
+++ src/main/java/launchcode/org/blogliftoff/BlogLiftoffApplication.java
@@ -0,0 +1,15 @@
+package launchcode.org.blogliftoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLiftoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogLiftoffApplication.class, args);
+	}
+
+}
+
+
